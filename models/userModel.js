f3e7da13d5b0a723d5734201e43eecbe53ba6d4d
shrutiujjwal24 <93506666+shrutiujjwal24@users.noreply.github.com>
2023-08-09 23:36:08 +0530

    i am

---
+++ models/userModel.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+
+//schema
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is require"],
+    },
+    lastName: {
+        type: String,
+
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        validate: validator.isEmail
+    },
+    password: {
+        type: String,
+        required: [true, 'password is require'],
+        minlength: [6, 'password should be greater than 6 character'],
+        select: true,
+    },
+    location: {
+        type: String,
+        default: "Gwalior",
+    },
+
+},
+    { timestamps: true }
+
+);
+//middelwares
+userSchema.pre("save", async function () {
+    if (!this.isModified) return;
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+//compare password
+userSchema.methods.comparePassword = async function (userPassword) {
+    const isMatch = await bcrypt.compare(userPassword, this.password);
+    return isMatch;
+}
+//JSON WEBTOKEN
+userSchema.methods.createJWT = function () {
+    return JWT.sign({ userId: this._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
+
+export default mongoose.model("User", userSchema);
---
+++ routes/userRoutes.js
@@ -0,0 +1,15 @@
+import express from "express"
+import { getUserController, updateUserController } from "../controllers/userController.js";
+import userAuth from "../middelwares/authMiddleware.js";
+
+//router object
+const router = express.Router();
+
+//routes
+// GET USERS DATA || POST
+router.post("/getUser", userAuth, getUserController);
+
+// UPDATE USER || PUT
+router.put("/update-user", userAuth, updateUserController);
+
+export default router;
